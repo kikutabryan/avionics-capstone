876010509041bd1424194b02db97e1ddc2f27c9a
Bryan Kikuta <112445285+kikutabryan@users.noreply.github.com>
2025-01-28 10:34:59 -0500

    elevation controller added and 2017 file

--- params.m
+++ params.m
@@ -1,5 +1,6 @@
 % Constants
 g = 9.805;  % [m/s^2]
+Elevation_MoI = 1.05; % Moment of Inertia constant to account for lever arm
 
 % Masses
 M_f = 0.713;  % [kg]
@@ -17,6 +18,7 @@ F_steady = 3.65;  % [N] - Force required to maintain steady level flight
 % Moments of inertia
 J_p = (M_b * L_h^2) + (M_f * L_h^2);  % [kg*m^2]
 J_t = (M_w * L_w^2) + ((M_b + M_f) * (L_h^2 + L_a^2));  % [kg*m^2]
+J_e = (((M_f + M_b)* L_a^2) + (M_w * L_w^2)) * Elevation_MoI;   % [kg*m^2]
 
 % Transfer functions
 dotP_deltaF_num = L_h;
@@ -30,6 +32,9 @@ dotLambda_P_den = [J_t, 0];
 
 ddotLambda_P_num = L_a * F_steady;
 ddotLambda_P_den = J_t;
+
+ddotE_F_Steady_num = L_a;
+ddotE_F_Steady_den = J_e;
 
 % PID limits
 deltaF_max = 3;  % [N]
